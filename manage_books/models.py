65e5c0332f04fd3935c2b1fd59c2613afa4da223
15010 <15010@eans-nt.edu.pl>
2026-05-27 15:58:30 +0200

    27.05

--- manage_books/models.py
+++ manage_books/models.py
@@ -3,20 +3,19 @@ import pytz
 
 # Create your models here.
 class Book(models.Model):
-
+    @property
+    def author(self):
+        return ', '.join([f"{author.first_name} {author.last_name}" for author in self.authors.all()])
+    
     COVERS = [
         ('hardcover', 'Hardcover'),
         ('paperback', 'Paperback'),
         ('ebook', 'E-book'),
         ('audiobook', 'Audiobook'),
-    ] 
-
-    LANGUAGE = [
+    ]
+    LANGUAGES = [
         ('english', 'English'),
-        ( 'polish', 'Polish'),
-        ('german', 'German'),
-        ('french', 'French'),
-        ('spanish', 'Spanish'),
+        ('polish', 'Polish'),
         ('hebrew', 'Hebrew'),
         ('other', 'Other'),
     ]
@@ -26,57 +25,51 @@ class Book(models.Model):
     publication_date = models.DateField()
     pages = models.IntegerField()
     cover = models.CharField(max_length=20, choices=COVERS)
-    language = models.CharField(max_length=20, choices=LANGUAGE)
+    language = models.CharField(max_length=20, choices=LANGUAGES)
     is_read = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
-    authors = models.ManyToManyField('Author', related_name='books')
-    publisher = models.ForeignKey('Publisher', on_delete=models.RESTRICT, on_update=models.CASCADE)
-    series = models.ForeignKey('Series', on_delete=models.RESTRICT, on_update=models.CASCADE, null=True, blank=True)
+    authors = models.ManyToManyField('Author', related_name='books', blank=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.RESTRICT)
+    series = models.ForeignKey('Series', on_delete=models.RESTRICT, blank=True, null=True)
     genres = models.ManyToManyField('Genre', related_name='books', blank=True)
-    topic = models.ManyToManyField('Topic', related_name='books', blank=True)
-    notes = models.ManyToOneField('Note', related_name='books', blank=True)
-
+    topics = models.ManyToManyField('Topic', related_name='books', blank=True)
+    
 class Author(models.Model):
-     TITLES = [
+    TITLES = [
         ('ks', 'Ks.'),
         ('dr', 'Dr.'),
         ('prof', 'Prof.'),
-        ('bp', 'Bp'),
+        ('bp', 'Bp.'),
     ]
-     
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    alias = models.CharField(max_length=100, blank=True)
+    alias = models.CharField(max_length=100, blank=True, null=True)
     nationality = models.CharField(max_length=100)
     title = models.CharField(max_length=50, choices=TITLES, blank=True, null=True)
-    books = models.ManyToManyField(Book, related_name='authors', blank=True)
-    series = models.ManyToManyField('Series', related_name='authors', blank=True)
-    
+
 class Publisher(models.Model):
-        name = models.CharField(max_length=200)
-        country = models.CharField(max_length=2, choises=pytz.country_names.items())
-        founded_year = models.IntegerField()
-        website = models.URLField(blank=True, null=True)
-        email = models.EmailField(blank=True, null=True)
-        books = models.ManyToOneField(Book, related_name='publishers', blank=True, null=True)
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=2, choices=pytz.country_names.items())
+    founded_year = models.IntegerField()
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
-class Genre(models.Model): 
-    name = models.CharField(max_length=100) 
+class Genre(models.Model): #gatunek (literacki)
+    name = models.CharField(max_length=100)
 
-class Series(models.Model):
+class Series(models.Model): #seria (książek)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    books = models.ManyToManyField(Book, related_name='series', blank=True)
     authors = models.ManyToManyField(Author, related_name='series', blank=True)
 
-class Topic(models.Model):
+class Topic(models.Model): #tematyka (książki)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    books = models.ManyToManyField(Book, related_name='topics', blank=True)
 
-class Note(models.Model): 
+class Note(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='notes')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     
